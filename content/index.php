e168509f9e91c9000c2153e305b3b3e6263d2f7a
luciano <lucianoadb@gmail.com>
2021-09-16 07:49:41 -0500

    cambios mobile

--- content/index.php
+++ content/index.php
@@ -38,7 +38,7 @@
         </div>
     </div>
 
-    <div class="row position-relative">
+    <div class="row position-relative d-none d-sm-flex">
         <img class="siguiente" src="../imagen/index/siguiente.png" alt="">
         <img class="anterior" src="../imagen/index/anterior.png" alt="">
         <div class="col-12 px-0">
@@ -61,10 +61,40 @@
             </table>
         </div>
     </div>
+    <div class="row position-relative d-flex d-sm-none">
+
+
+        <div class="col-12 px-0">
+            <table class="caruselfalso">
+                <tbody>
+                    <tr>
+                        <td class="tg-0pky"><img src="../imagen/index/hazloreal.png" alt=""></td>
+                        <td class="tg-0pky"><img class="siguiente" src="../imagen/index/siguiente.png" alt=""></td>
+                    </tr>
+                    <tr>
+                        <td class="tg-0pky"><img src="../imagen/index/iwon.png" alt=""></td>
+                        <td class="tg-0pky" rowspan="2"><img src="../imagen/index/faswet.png" alt=""></td>
+                    </tr>
+                    <tr>
+                        <td class="tg-0pky"><img src="../imagen/index/p2w.png" alt=""></td>
+                    </tr>
+                    <tr>
+                        <td class="tg-0pky"><img src="../imagen/index/LandingP2W.png" alt=""></td>
+                        <td class="tg-0pky"><img src="../imagen/index/raro.png" alt=""></td>
+                    </tr>
+                    <tr>
+                        <td class="tg-0pky"> <img class="anterior" src="../imagen/index/anterior.png" alt=""></td>
+                        <td class="tg-0pky"><img src="../imagen/index/nasbi.png" alt=""></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 
     <div class="row contenedor-marcas">
         <div class="col-12 text-center">
-            <img src="../imagen/index/pasarela.png" alt="">
+            <img class="d-none d-sm-block" src="../imagen/index/pasarela.png" alt="">
+            <img class="d-sm-none d-block w-100" src="../imagen/index/pasarelaresponsive.png" alt="">
         </div>
     </div>
 
@@ -82,7 +112,7 @@
                 </h2>
             </div>
         </div>
-        <div class="col-sm-4 offset-sm-6 ">
+        <div class="col-sm-4 offset-sm-6 responsivepadding">
             <div class="rayaverdemix"></div>
             <h3 class="actuamos">
                 Actuamos en función de <br> tu marca,por eso decimos <br> think+create+act
@@ -102,21 +132,16 @@
                 TALENTO
             </h2>
         </div>
-        <div class="col-2">
+        <div class="col-2 d-sm-block d-none">
             <div class="nav flex-column nav-pills" id="porfolio" role="tablist" aria-orientation="vertical">
-                <a class="nav-link active" id="Branding-tab" data-toggle="pill" href="#Branding" role="tab"
-                    aria-controls="Branding" aria-selected="true">BRANDING</a>
-                <a class="nav-link" id="BTL-tab" data-toggle="pill" href="#BTL" role="tab" aria-controls="BTL"
-                    aria-selected="false">BTL</a>
-                <a class="nav-link" id="Production-tab" data-toggle="pill" href="#Production" role="tab"
-                    aria-controls="Production" aria-selected="false">PRODUCTION</a>
-                <a class="nav-link" id="UX/UI-tab" data-toggle="pill" href="#UX-UI" role="tab" aria-controls="UX/UI"
-                    aria-selected="false">UX / UI</a>
-                <a class="nav-link" id="Web-Site-tab" data-toggle="pill" href="#Web-Site" role="tab"
-                    aria-controls="Web-Site" aria-selected="false">WEB SITE</a>
+                <a class="nav-link active" id="Branding-tab" data-toggle="pill" href="#Branding" role="tab" aria-controls="Branding" aria-selected="true">BRANDING</a>
+                <a class="nav-link" id="BTL-tab" data-toggle="pill" href="#BTL" role="tab" aria-controls="BTL" aria-selected="false">BTL</a>
+                <a class="nav-link" id="Production-tab" data-toggle="pill" href="#Production" role="tab" aria-controls="Production" aria-selected="false">PRODUCTION</a>
+                <a class="nav-link" id="UX/UI-tab" data-toggle="pill" href="#UX-UI" role="tab" aria-controls="UX/UI" aria-selected="false">UX / UI</a>
+                <a class="nav-link" id="Web-Site-tab" data-toggle="pill" href="#Web-Site" role="tab" aria-controls="Web-Site" aria-selected="false">WEB SITE</a>
             </div>
         </div>
-        <div class="col-6">
+        <div class="col-6 d-sm-block d-none">
             <div class="tab-content" id="porfolio">
                 <div class="tab-pane fade show active" id="Branding" role="tabpanel" aria-labelledby="Branding-tab">
 
@@ -179,8 +204,7 @@
                             <img class="" src="../imagen/index/bilb.png" alt="">
                             <div class="rayaverdemix my-3"></div>
                             <p>
-                                <b>Pensamiento</b> <br> Acciones estratégicas <br> para que logres <br> experiencias
-                                únicas.
+                                <b>Pensamiento</b> <br> Acciones estratégicas <br> para que logres <br> experiencias únicas.
                             </p>
 
                         </div>
@@ -228,8 +252,7 @@
                             <img class="" src="../imagen/index/chat-empty.png" alt="">
                             <div class="rayaverdemix my-3"></div>
                             <p>
-                                <b>Metodologías</b> <br> Nos aseguramos <br> de usar la metodología <br> adecuada para
-                                tu proyecto
+                                <b>Metodologías</b> <br> Nos aseguramos <br> de usar la metodología <br> adecuada para tu proyecto
 
                             </p>
                         </div>
@@ -259,8 +282,7 @@
                             <img class="" src="../imagen/index/check-circle.png" alt="">
                             <div class="rayaverdemix my-3"></div>
                             <p>
-                                <b>Ventas</b> <br> Generación de leads y ventas, <br> además ofrecemos <br> soporte a
-                                tus clientes
+                                <b>Ventas</b> <br> Generación de leads y ventas, <br> además ofrecemos <br> soporte a tus clientes
 
                             </p>
                         </div>
@@ -289,6 +311,212 @@
             </div>
             <img class="elipsimix" src="../imagen/index/Elipse 6.png" alt="">
         </div>
+        <div class="col-6 d-sm-none d-block">
+
+            <div class="accordion" id="accordionExample">
+                <div class="card-header" id="headingOne">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                            BRANDING
+                        </button>
+                    </h2>
+
+                    <div id="collapseOne" class="collapse " aria-labelledby="headingOne" data-parent="#accordionExample">
+                        <div class="row pl-5">
+                            <div class="col-6">
+                                <img class="" src="../imagen/index/trophy.png" alt="">
+                                <div class="rayaverdemix my-3"></div>
+                                <p>
+                                    <b>Visibilidad</b> <br> Logra resaltar <br> el poder de tu marca
+                                </p>
+
+                            </div>
+                            <div class="col-6">
+                                <img class="" src="../imagen/index/heart.png" alt="">
+                                <div class="rayaverdemix my-3"></div>
+                                <p>
+                                    <b>Awareness</b> <br> Recordación <br> de tus productos
+                                </p>
+
+                            </div>
+                            <div class="col-6">
+                                <img class="" src="../imagen/index/star.png" alt="">
+                                <div class="rayaverdemix my-3"></div>
+                                <p>
+                                    <b>Posicionamiento</b> <br> Construcción de marca
+                                </p>
+
+                            </div>
+                            <div class="col-6">
+                                <img class="" src="../imagen/index/flag_1.png" alt="">
+                                <div class="rayaverdemix my-3"></div>
+                                <p>
+                                    <b>Eventos</b> <br> Logra impacto directo <br> y creativo con tu mensaje
+                                </p>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="card-header" id="headingTwo">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            BTL
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                    <div class="row pl-5">
+                        <div class="col-6">
+                            <img class="" src="../imagen/index/bookmark.png" alt="">
+                            <div class="rayaverdemix my-3"></div>
+                            <p>
+                                <b>Eventos</b> <br> Logra impacto directo <br> y creativo con tu mensaje
+                            </p>
+                        </div>
+                        <div class="col-6">
+                            <img class="" src="../imagen/index/hearts-alt.png" alt="">
+                            <div class="rayaverdemix my-3"></div>
+                            <p>
+                                <b>Experiencias</b> <br> Fidelizar y crear vínculos <br> con los seguidores de tu marca
+
+                            </p>
+
+                        </div>
+                        <div class="col-6">
+                            <img class="" src="../imagen/index/bilb.png" alt="">
+                            <div class="rayaverdemix my-3"></div>
+                            <p>
+                                <b>Pensamiento</b> <br> Acciones estratégicas <br> para que logres <br> experiencias únicas.
+                            </p>
+
+                        </div>
+
+
+                    </div>
+                </div>
+                <div class="card-header" id="headingThree">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Production
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                    <div class="row pl-5">
+                        <div class="col-6">
+                            <img class="" src="../imagen/index/bolt_1.png" alt="">
+                            <div class="rayaverdemix my-3"></div>
+                            <p>
+                                <b>Impacto</b> <br> Logra que tu mensaje <br> audiovisual impacte <br> y genere ventas
+
+                            </p>
+                        </div>
+                        <div class="col-6">
+                            <img class="" src="../imagen/index/tetris02.png" alt="">
+                            <div class="rayaverdemix my-3"></div>
+                            <p>
+                                <b>Experiencias</b> <br> Arte y fotografía se unen<br> para crear experiencias visuales
+                            </p>
+
+                        </div>
+                        <div class="col-6">
+                            <img class="" src="../imagen/index/incongnito.png" alt="">
+                            <div class="rayaverdemix my-3"></div>
+                            <p>
+                                <b>Visión</b> <br> Llevamos tu idea <br> de negocio a la realidad
+
+                            </p>
+
+                        </div>
+
+
+                    </div>
+                </div>
+                <div class="card-header" id="headingfour">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapsefour" aria-expanded="false" aria-controls="collapsefour">
+                            UX/UI-tab
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapsefour" class="collapse" aria-labelledby="headingfour" data-parent="#accordionExample">
+                    <div class="row pl-5">
+                        <div class="col-6">
+                            <img class="" src="../imagen/index/chat-empty.png" alt="">
+                            <div class="rayaverdemix my-3"></div>
+                            <p>
+                                <b>Metodologías</b> <br> Nos aseguramos <br> de usar la metodología <br> adecuada para tu proyecto
+
+                            </p>
+                        </div>
+                        <div class="col-6">
+                            <img class="" src="../imagen/index/joystick.png" alt="">
+                            <div class="rayaverdemix my-3"></div>
+                            <p>
+                                <b>Design</b> <br>Soluciones empáticas<br> y minimalistas
+
+                            </p>
+
+                        </div>
+                        <div class="col-6">
+                            <img class="" src="../imagen/index/crown.png" alt="">
+                            <div class="rayaverdemix my-3"></div>
+                            <p>
+                                <b>Development</b> <br>Interfaces y experiencia <br> del usuario
+
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="card-header" id="headingfive">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapsefive" aria-expanded="false" aria-controls="collapsefive">
+                            Web Site
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapsefive" class="collapse" aria-labelledby="headingfive" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <div class="row pl-5">
+                            <div class="col-6">
+                                <img class="" src="../imagen/index/check-circle.png" alt="">
+                                <div class="rayaverdemix my-3"></div>
+                                <p>
+                                    <b>Ventas</b> <br> Generación de leads y ventas, <br> además ofrecemos <br> soporte a tus clientes
+
+                                </p>
+                            </div>
+                            <div class="col-6">
+                                <img class="" src="../imagen/index/cube.png" alt="">
+                                <div class="rayaverdemix my-3"></div>
+                                <p>
+                                    <b>Construcción</b> <br>Describe tus fortalezas<br> y construye tu reputación
+
+
+                                </p>
+
+                            </div>
+                            <div class="col-6">
+                                <img class="" src="../imagen/index/ox.png" alt="">
+                                <div class="rayaverdemix my-3"></div>
+                                <p>
+                                    <b>Planning</b> <br>Planificamos, creamos, <br> implementamos y realizamos
+                                    <br>mantenimiento a sitios web
+
+                                </p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
     </div>
     <div style="height: 228px">
 
@@ -303,8 +531,7 @@
                             BLOG
                         </h2>
                         <div class="rayaverdemix my-4"></div>
-                        <p>Los seres humanos amamos lo visual, es un hecho <br> innegable que se ve reflejado en cada
-                            desarrollo o <br> adelanto que conseguimos posicionar.</p>
+                        <p>Los seres humanos amamos lo visual, es un hecho <br> innegable que se ve reflejado en cada desarrollo o <br> adelanto que conseguimos posicionar.</p>
                     </div>
                     <div class="col-sm-7">
                         <div class="container-carousel">
@@ -316,8 +543,7 @@
                                     El video como <br> herramienta <br> de Marketing
                                 </h6>
                                 <div class="rayaverdemix my-4"></div>
-                                <p>Los seres humanos amamos lo visual, es un hecho innegable que se ve reflejado en cada
-                                    desarrollo o adelanto que conseguimos posicionar.</p>
+                                <p>Los seres humanos amamos lo visual, es un hecho innegable que se ve reflejado en cada desarrollo o adelanto que conseguimos posicionar.</p>
 
                             </div>
                             <div class="carusel">
@@ -327,8 +553,7 @@
                                     El video como <br> herramienta <br> de Marketing
                                 </h6>
                                 <div class="rayaverdemix my-4"></div>
-                                <p>Los seres humanos amamos lo visual, es un hecho innegable que se ve reflejado en cada
-                                    desarrollo o adelanto que conseguimos posicionar.</p>
+                                <p>Los seres humanos amamos lo visual, es un hecho innegable que se ve reflejado en cada desarrollo o adelanto que conseguimos posicionar.</p>
 
                             </div>
                         </div>
@@ -365,11 +590,11 @@
 <?php include '../include/include-js.php';?>
 
 <script>
-$(window).ready(function() {
-    $(document.body).on('click', "#higado", function() {
-        $("#content-pages-rutas").load('video_marketing.php');
+    $(window).ready(function() {
+        $(document.body).on('click', "#higado", function() {
+            $("#content-pages-rutas").load('video_marketing.php');
+        });
     });
-});
 </script>
 
 </html>
