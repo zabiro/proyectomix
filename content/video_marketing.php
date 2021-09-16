281ebb045b2fc7520667a1d8ec2c3f892014fecd
he03villa <he03villa@gmail.com>
2021-09-16 00:49:44 -0500

    termine

--- content/video_marketing.php
+++ content/video_marketing.php
@@ -1,8 +1,64 @@
-<div class="row">
-    <div class="col-12">
-        <a class="btn cerrar">cerrar x</a>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <a class="btn cerrar">cerrar x</a>
+        </div>
+        <div class="col-7">
+            <h1 class="titulo-modal">El video como herramienta de Marketing</h1>
+        </div>
     </div>
-    <div class="col-12">
-        <h1 class="titulo-modal">El video como herramienta de Marketing</h1>
+    <div class="row">
+        <div class="col-12">
+            <img src="../imagen/video_marketing/shutterstock.png" class="img-modal" alt="">
+        </div>
+        <div class="col-3">
+            <div class="content-fundador">
+                <img src="../imagen/video_marketing/fundador.png" alt="">
+                <div>
+                    <label for="">Juan Diego varon</label>
+                    <span>fundador</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-9">
+            <p class="parrafo-modal">
+                <b>Los seres humanos amamos lo visual, es un hecho innegable que se ve reflejado en cada desarrollo o adelanto que conseguimos posicionar.</b><br><br>
+                Desde la creación en el pasado de murales que narraban nuestras primeras historias, pasando por el realismo en la pintura y luego en la escultura, hemos querido plasmar cada una de nuestras actividades para que perduren y sean recordadas por las generaciones venideras.<br><br> Luego, con la llegada de todas las técnicas gráficas generadas por el advenimiento de la era informática combinadas con el cine, llevamos esto un paso más allá de lo que habíamos tan siquiera imaginado.<br><br> Más adelante aparece el uso de las enseñanzas cinematográficas para llevar a todo el mundo, de la mejor forma, la ventaja de nuestros productos, narrados de tal manera que el consumidor se le haga casi imposible rechazar nuestro mensaje.
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <label for="" class="numero-modal">90 MIL</label>
+        </div>
+        <div class="col-12">
+            <p class="parrafo-modal parrafo2-modal">
+                <b>Los seres humanos amamos lo visual, es un hecho innegable que se ve reflejado en cada desarrollo o adelanto que conseguimos posicionar.</b><br><br>
+                Desde la creación en el pasado de murales que narraban nuestras primeras historias, pasando por el realismo en la pintura y luego en la escultura, hemos querido plasmar cada una de nuestras actividades para que perduren y sean recordadas por las generaciones venideras.<br><br> Luego, con la llegada de todas las técnicas gráficas generadas por el advenimiento de la era informática combinadas con el cine, llevamos esto un paso más allá de lo que habíamos tan siquiera imaginado.<br><br> Más adelante aparece el uso de las enseñanzas cinematográficas para llevar a todo el mundo, de la mejor forma, la ventaja de nuestros productos, narrados de tal manera que el consumidor se le haga casi imposible rechazar nuestro mensaje.
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <img src="../imagen/video_marketing/shutterstock.png" class="img-modal-grupo" alt="">
+            <span class="fecha">17 abril, 2021</span>
+            <label class="label-modal-grupo" for="">El video como herramienta de Marketing.</label>
+            <div class="raya-bottom"></div>
+            <p class="parrajo-modal-grupo">Los seres humanos amamos lo visual, es un hecho innegable que se ve reflejado en cada desarrollo o adelanto que conseguimos posicionar.</p>
+        </div>
+        <div class="col-4">
+            <img src="../imagen/video_marketing/shutterstock.png" class="img-modal-grupo" alt="">
+            <span class="fecha">17 abril, 2021</span>
+            <label class="label-modal-grupo" for="">El video como herramienta de Marketing.</label>
+            <div class="raya-bottom"></div>
+            <p class="parrajo-modal-grupo">Los seres humanos amamos lo visual, es un hecho innegable que se ve reflejado en cada desarrollo o adelanto que conseguimos posicionar.</p>
+        </div>
+        <div class="col-4">
+            <img src="../imagen/video_marketing/shutterstock.png" class="img-modal-grupo" alt="">
+            <span class="fecha">17 abril, 2021</span>
+            <label class="label-modal-grupo" for="">El video como herramienta de Marketing.</label>
+            <div class="raya-bottom"></div>
+            <p class="parrajo-modal-grupo">Los seres humanos amamos lo visual, es un hecho innegable que se ve reflejado en cada desarrollo o adelanto que conseguimos posicionar.</p>
+        </div>
     </div>
 </div>
