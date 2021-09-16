b5afb974b1348cf6e9417fe511604634f6a45cd4
luciano <lucianoadb@gmail.com>
2021-09-16 00:46:03 -0500

    terminado footer

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,65 @@
-
-<div class="row">
+<div class="row row-footer-varon">
+    <p class="thinkfooter">think + <br> create + <br> act</p>
     <div class="col-12">
-        <p>Include Footer</p>
+        <div class="container">
+            <div class="row my-5">
+                <div class="col-sm-5 contenedor-footer">
+                    <div>
+
+                        <h2>contacto</h2>
+                        <div class="rayanegramix mt-5 mb-3"></div>
+                        <p><b> New York. </b> <br> 119 Peter st. <br> Zip code: 10029 <br> +1 (347) 255-3268</p>
+
+                    </div>
+                    <div>
+                        <div class="rayanegramix mt-5 mb-3"></div>
+                        <p><b> Bogotá. </b> <br> Calle 93 #12-78 of 601 <br> (571) 3057051210</p>
+                    </div>
+
+                    <div class="my-5">
+                        <h3>info@varonstudio.com</h3>
+                        <h3>3057051210</h3>
+
+                    </div>
+                    <div class="my-5 contenedor-redes-footer">
+                        <i class="fab fa-facebook-f"></i>
+                        <i class="fab fa-behance"></i>
+                        <i class="fab fa-instagram"></i>
+                        <i class="fab fa-linkedin-in"></i>
+                        <i class="fab fa-youtube"></i>
+                    </div>
+
+                </div>
+                <div class="col-sm-7">
+                    <form action="">
+                        <div class="row row-formulario-varon">
+                            <div class="col-sm-6">
+
+                                <label for="">Nombre</label>
+                                <input type="text" class="form-control" placeholder="">
+                            </div>
+                            <div class="col-sm-6">
+
+                                <label for="">Email</label>
+                                <input type="text" class="form-control" placeholder="">
+                            </div>
+                            <div class="col-12">
+
+                                <label for="">Mensaje</label>
+
+                                <textarea class="form-control" name="" id="" rows="3"></textarea>
+
+                            </div>
+                            <div class="col-12">
+
+                                <button type="button" class="btn btn-outline-primary px-5">ENVIAR</button>
+                            </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+
     </div>
+</div>
 </div>
